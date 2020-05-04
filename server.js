b6952bcb5c7f6c3b1c66ffe53e5540ef260677d3
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-05-04 22:51:33 +0000

    Versão 2

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const server = express();
 
+// tratar as requisições HTTP (GET, POST, PUT, DELETE)
+
 server.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send('Hello World, Fatec!');
 })
 
+// escutar um porta com as requisições HTTP:
 server.listen(process.env.PORT || 3000);
